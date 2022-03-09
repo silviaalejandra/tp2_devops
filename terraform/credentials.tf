52cbee15aba6bb0d02306fac447c7090842b955b
silviaalejandra <97700312+silviaalejandra@users.noreply.github.com>
2022-03-09 12:03:11 -0300

    Se eliminan credenciales

--- terraform/credentials.tf
+++ terraform/credentials.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
   features {}
 
-  subscription_id   = "2e3b5939-31bd-4d94-8053-5f2b29b1b99c"
-  tenant_id         = "899789dc-202f-44b4-8472-a6d40f9eb440"
-  client_id         = "ab23ce50-de19-4394-8db8-2549f3280f9d"
-  client_secret     = "JT-Rb9-d.RlXNMYQML3PV4KHGH~8oQyLMt"
+  subscription_id = ${ARM_SUBSCRIPTION_ID}
+  tenant_id       = ${ARM_TENANT_ID}
+  client_id       = ${ARM_CLIENT_ID}
+  client_secret   = ${ARM_CLIENT_SECRET}
 }

3e0c95cd5cd16f6ba291e5df6f2fa85b1be21c77
silviaalejandra <silviaalejandra@gmail.com>
2022-03-11 17:08:28 -0300

    correcciones a los seteos del controller

--- ansible/controller.sh
+++ ansible/controller.sh
@@ -7,4 +7,18 @@
 # *** 2- Instalacion chrony
 # *** 3- Seteo /etc/hosts local con nodos Azure
 #--------------------------------------------------
-ansible-playbook -i hosts -l controller setup_controller.yaml -v
+strDate=`date "+%Y%m%d%H%M%S"`
+# Chequeo de fingerprint de llave  = falso
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+if [ ! -n "$strLog" -o ! -e $strLog ]
+then
+	strLog="controller$strDate.log"
+fi
+
+# controlo salida por pantalla y log
+exec > >(tee ${strLog} 2>&1)
+
+echo `date "+%d/%m/%Y %H:%M:%S"`": Inicia seteo de controlador con ansible"
+ansible-playbook -i hosts -l controller setup_controller.yaml -v #--ssh-common-args='-o StrictHostKeyChecking=no'
+echo `date "+%d/%m/%Y %H:%M:%S"`": Finaliza seteo de controlador con ansible"

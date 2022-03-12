2c1a2f8503a2c7e533577307de12adc25f4e09e0
silviaalejandra <silviaalejandra@gmail.com>
2022-03-11 23:58:53 -0300

    Prueba con un nodo y un worker

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,22 +24,24 @@ variable "environment" {
   default = "TP_02_SILGONZA" 
 }
 
-variable "vm_size" { # Deseable 2 CPU, 4 GB, 1x20 HDD
+variable "vm_size" { # NFS Deseable 2 CPU, 4 GB, 1x20 HDD
   type = string
   description = "Tamaño de la máquina virtual generica"
-  default = "Standard_A2_v2" # 2 CPU, 4 GB, 20GB HDD 
+  default = "Standard_F1" 
+}
+#Standard_F1    1 CPU, 2 GB, 16GB HDD 
+#Standard_B2ms  2 CPU, 8 GB, 16GB HDD 
+#Standard_A2_v2 2 CPU, 4 GB, 20GB HDD 
+variable "vm_size_b" { # MASTER Deseable 2 CPU, 8 GB, 1x20 HDD
+  type = string
+  description = "Tamaño de la máquina virtual generica"
+  default = "Standard_B2ms" 
 }
 
-variable "vm_size_b" { # Deseable 2 CPU, 8 GB, 1x20 HDD
+variable "vm_size_c" { # WORKERS Deseable 2 CPU, 4 GB, 1x20 HDD
   type = string
   description = "Tamaño de la máquina virtual generica"
-  default = "Standard_B2ms" # 2 CPU, 8 GB, 16GB HDD 
-}
-
-variable "vm_size_c" { # Deseable 2 CPU, 8 GB, 1x20 HDD
-  type = string
-  description = "Tamaño de la máquina virtual generica"
-  default = "Standard_F1" # 2 CPU, 8 GB, 16GB HDD 
+  default = "Standard_A2_v2" 
 }
 
 # max 9 valores
@@ -60,5 +62,6 @@ variable "vm_count_b" { # Listado de VMs a crear - todas mismo tamanio generico
 variable "vm_count_c" { # Listado de VMs a crear - todas mismo tamanio generico
   description = "Lista de maquinas virtuales a crear tipo C"
   type = list(string)
-  default = ["worker01", "worker02"]
+  #default = ["worker01", "worker02"]
+  default = ["worker01"]
 }
